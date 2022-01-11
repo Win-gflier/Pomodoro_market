a1759f927873dbf2d2b91e676c7b7afe0c312b22
Zaven17 <ztadevos@yahoo.com>
2022-01-12 01:05:35 +0400

    temp commit

--- app/src/main/java/com/example/pomodorolike/ui/SplashActivity.kt
+++ app/src/main/java/com/example/pomodorolike/ui/SplashActivity.kt
@@ -1,15 +1,20 @@
 package com.example.pomodorolike.ui
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import com.example.pomodorolike.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        setStatusBar()
         goToMain()
 
     }
@@ -18,6 +23,16 @@ class SplashActivity : AppCompatActivity() {
             val mIntent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(mIntent)
             finish()
-        }, 2000)
+        },3000)
+    }
+    private fun setStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            this.window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            this.window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            this.window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            this.window.statusBarColor =
+                ContextCompat.getColor(this, R.color.white)
+        }
     }
 }
